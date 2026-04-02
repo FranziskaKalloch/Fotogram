893475180178d6136f106fe14c5eb7bc486f2211
Franziska Kalloch <franzy@MacBook-Air-von-Franziska.fritz.box>
2026-04-02 19:31:45 +0200

    feat: function overlay declared

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 let currentIndex = 0;
-const basePath = "assets/img/romy/";
-const dialogRef = document.getElementById("imgDialog");
+const basePath = 'assets/img/romy/';
+const dialogRef = document.getElementById('imgDialog');
 
 const twelveMonthImg = [
-  "Geburt.jpeg",
-  "1-Monat.jpeg",
-  "2-Monate.jpeg",
-  "3-Monate.jpeg",
-  "4-Monate.jpeg",
-  "5-Monate.jpeg",
-  "6-Monate.jpeg",
-  "7-Monate.jpeg",
-  "8-Monate.jpeg",
-  "9-Monate.jpeg",
-  "10-Monate.jpeg",
-  "11-Monate.jpeg",
-  "1-Jahr.jpeg",
+  'Geburt.jpeg',
+  '1-Monat.jpeg',
+  '2-Monate.jpeg',
+  '3-Monate.jpeg',
+  '4-Monate.jpeg',
+  '5-Monate.jpeg',
+  '6-Monate.jpeg',
+  '7-Monate.jpeg',
+  '8-Monate.jpeg',
+  '9-Monate.jpeg',
+  '10-Monate.jpeg',
+  '11-Monate.jpeg',
+  '1-Jahr.jpeg',
 ];
 
 function init() {
@@ -23,8 +23,8 @@ function init() {
 }
 
 function render() {
-  const imgContainerRef = document.getElementById("img-container");
-  imgContainerRef.innerHTML = "";
+  const imgContainerRef = document.getElementById('img-container');
+  imgContainerRef.innerHTML = '';
 
   for (let i = 0; i < twelveMonthImg.length; i++) {
     imgContainerRef.innerHTML += `
@@ -48,14 +48,14 @@ function stopMyPropagation(event) {
 }
 
 function renderDialog() {
-  const pictureRef = document.getElementById("dialogPicture");
+  const pictureRef = document.getElementById('dialogPicture');
   pictureRef.innerHTML = `
   <img class="img" src="${basePath}${twelveMonthImg[currentIndex]}">`;
 
-  const fileNameRef = document.getElementById("fileName");
+  const fileNameRef = document.getElementById('fileName');
   fileNameRef.innerHTML = twelveMonthImg[currentIndex];
 
-  const counterRef = document.getElementById("counter");
+  const counterRef = document.getElementById('counter');
   counterRef.textContent = `${currentIndex + 1} / ${twelveMonthImg.length}`;
 }
 
